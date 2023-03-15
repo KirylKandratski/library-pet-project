07afc58e5f4701f7972cb757ee963d09b728b801
KirylKandratski <kiryl.kandratski@gmail.com>
2023-03-15 13:36:31 +0100

    feat: add DatabaseConfig, LibraryAppInitializer and WebConfig

---
+++ src/main/java/kandratski/library/configuration/DatabaseConfig.java
@@ -0,0 +1,46 @@
+package kandratski.library.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:database.properties")
+public class DatabaseConfig {
+
+    @Value("${url}")
+    private String url;
+
+    @Value("${user}")
+    private String user;
+
+    @Value("${password}")
+    private String password;
+
+    @Value("${driver}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.setResultsMapCaseInsensitive(true);
+        return jdbcTemplate;
+    }
+}
