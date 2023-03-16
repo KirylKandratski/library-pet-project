42e4cd51146a5c715e9ffdc167f32b83c9900031
KirylKandratski <kiryl.kandratski@gmail.com>
2023-03-16 14:48:42 +0100

    feat: add Book, BookDAO, BooksController, view books/getAll.html

---
+++ src/main/java/kandratski/library/dao/BookDAO.java
@@ -0,0 +1,26 @@
+package kandratski.library.dao;
+
+import kandratski.library.models.Book;
+import kandratski.library.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> getAll() {
+        return jdbcTemplate.query("SELECT * FROM Book",
+                new BeanPropertyRowMapper<>(Book.class));
+    }
+}
